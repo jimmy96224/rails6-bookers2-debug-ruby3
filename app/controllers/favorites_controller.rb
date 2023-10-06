fc9579a81148f1d05493e49751b50d1f8ef432a4
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-10-06 16:23:52 +0000

    [wip]advanced task5

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,17 +3,13 @@ class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
-    session[:previous_url] = request.referer
     favorite.save
-    redirect_to session[:previous_url]
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
-    session[:previous_url] = request.referer
     favorite.destroy
-    redirect_to  session[:previous_url]
   end
   
 end
